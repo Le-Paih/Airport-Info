1b3e431959b98b5c06a14170db3b7a721bbe4ea4
Le-Paih <luis.lepaih@gmail.com>
2024-04-24 19:57:42 +0100

    Intro Update

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,21 +1,6 @@
 function Intro() {
   return (
-    <div className="dark:text-dWhite mx-10 mt-10 text-center text-sm">
-      <p>
-        <br />
-        <b>Explore Airport Locations: </b>
-        <br /> Discover airports across the globe with our extensive database.
-        Simply enter the name or code of the airport you&apos;re interested in,
-        and instantly uncover its precise location on the map.
-      </p>
-      <p>
-        <br /> <b>Check Weather Conditions: </b>
-        <br />
-        Stay informed about current weather conditions at any airport. Get
-        real-time updates on temperature, wind speed, humidity, and more,
-        ensuring you&apos;re prepared for your journey ahead.
-      </p>
-
+    <div className="mx-10 mt-10 text-center text-sm dark:text-dWhite">
       <p>
         <br /> <b>How to Use:</b>
         <br /> Enter the name, IATA code or the country of the airport you wish
